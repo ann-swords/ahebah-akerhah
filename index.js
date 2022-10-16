fd3ae15df8d09ee6b9a404720af40f3a04a2caed
Anwaar <anwaaralsyouf@gmail.com>
2022-10-16 09:18:38 +0300

    set up ejs express node project

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+//Middleware
+app.set('view engine', 'ejs')
+
+//Routes
+
+
+
+
+
+
+
+
+
+//Listener
+app.listen(PORT,() => {
+    console.log("You're running the Ahebah, Akerhah App")
+})
