1a846a77ab2dc008e9cd9750e25bc9c7891d1b9e
Anwaar <anwaaralsyouf@gmail.com>
2022-10-16 10:14:19 +0300

    favourite animals complete

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+//Importing express and ejsLayouts
 const express = require('express')
 const app = express()
+const ejsLayouts = require('express-ejs-layouts')
+
 const PORT = 3000
 
 //Middleware
 app.set('view engine', 'ejs')
+app.use(ejsLayouts)
 
 //Routes
 
+app.get('/', (req, res) =>{
+    res.render('home.ejs')
+})
 
-
-
-
-
+//fav animals
+app.get('/animals', (req,res)=>{
+    const favAnimals = ['cat the meow', 'dog', 'rabbit', 'horse' ]
+    res.render('animals.ejs', {animals: favAnimals })
+})
 
 
 
